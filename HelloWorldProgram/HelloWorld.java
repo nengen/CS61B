f2e79d8ef93f2d32aa1ef83b6695c72d6c9b0086
Nils-erik Engen <engennilserik@gmail.com>
2017-04-25 14:49:21 +0200

    Hello world program from lecture 1

--- HelloWorldProgram/HelloWorld.java
+++ HelloWorldProgram/HelloWorld.java
@@ -1,4 +1,4 @@
-public class HelloWorld{
+public class HelloWorld {
   public static void main(String[] args){
     System.out.println("Hello world!");
     //before you can use a var, you must declare it
