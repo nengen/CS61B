fcf7b53484a7257900df3d354924fa2e0c74405f
Nils-erik Engen <engennilserik@gmail.com>
2017-04-25 14:42:25 +0200

    Larger program from lecture 1

---
+++ Largerprogram/Larger.java
@@ -0,0 +1,20 @@
+public class Larger{
+
+  //def is almost equivalent to public static
+  //Functions need declared types as well.
+  //Functions can only return one thing
+  //Must declare types of the parameters
+  public static int larger(int x, int y) {
+    if (x > y) {
+      return x;
+      System.out.println(x);
+    }
+    return y;
+    System.out.println(y);
+  }
+
+  public static void main(String[] args) {
+    System.out.println(larger(5, 10));
+  }
+
+}
