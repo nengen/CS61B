dcf1ea692f39d8786ca7769ebf3b00a6f509b619
Nils-erik Engen <engennilserik@gmail.com>
2017-04-30 16:31:08 +0200

    Updated homework from week 1, with comments

--- maximumValueOfArray/maxValue.java
+++ maximumValueOfArray/maxValue.java
@@ -1,4 +1,15 @@
 public class maxValue {
+	/*
+	* Max function
+	* Takes an array and returns an integer
+	* While loop, loops through the array
+	* Checks if the next number is larger than
+	the largest so far
+	* If it is the largest then we assign it to
+	the largest variable, if not we don't do anything
+	* Then we increment the counter i.
+	* The method returns the largest number
+	*/
 	public static int max(int[] m){
 		int i = 0;
 		int largest = m[0];
@@ -10,6 +21,13 @@ public class maxValue {
 		}
 		return largest;
 	}
+	/*
+	* Main method
+	* creates a new array called numbers which contains
+	7 random numbers
+	* Then we print the result of using the max method
+	on the numbers array
+	*/
 	public static void main(String[] args) {
 		int[] numbers = new int[]{9, 2, 15, 2, 22, 10, 8};
 		System.out.println(max(numbers));
